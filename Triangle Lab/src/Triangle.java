ff874bd1b36312ccfd51bacbdd11227a64c3a02d
SolveForX_ <86010571+SolveForX24@users.noreply.github.com>
2022-04-05 08:14:10 -0400

    Comments, minor optimization. Lab done

--- Triangle Lab/src/Triangle.java
+++ Triangle Lab/src/Triangle.java
@@ -1,9 +1,9 @@
 /*
-* Name: Xander Russell
-* Date: 3/30/22
-* Lab: Triangle Lab
-* Extra: The getting side lengths and calculating area processes in main will loop until the user wants to stop.
-* */
+ * Name: Xander Russell
+ * Date: 3/30/22
+ * Lab: Triangle Lab
+ * Extra: The getting side lengths and calculating area processes in main will loop until the user wants to stop.
+ * */
 
 import java.util.Scanner;
 import java.lang.Math.*;
@@ -21,14 +21,17 @@ public class Triangle
         sideB = b;
         sideC = c;
     }
-
+    
+    // Returns side variables added together.
     public void calcPerimeter( )
     {
         perimeter = sideA + sideB + sideC;
     }
-
+    
+    // Returns area with half of perimeter calculated.
     public double getArea( )
     {
+        calcPerimeter();
         double s = perimeter/2;
         return Math.sqrt(s * ((s - sideA) * (s - sideB) * (s - sideC)));
     }
